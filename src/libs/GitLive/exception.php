916d7a67acb19c665fe495a078fc3e92fc14b82c
Suzunone <suzunone.eleven@gmail.com>
2016-06-23 19:21:10 +0900

    mod exception

---
+++ src/libs/GitLive/exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @category   GitCommand
+ * @package    GitLive
+ * @subpackage GitLiveFlow
+ * @author     akito<akito-artisan@five-foxes.com>
+ * @author     suzunone<suzunone.eleven@gmail.com>
+ * @copyright Project Git Live
+ * @license MIT
+ * @version    GIT: $Id$
+ * @link https://github.com/Git-Live/git-live
+ * @see https://github.com/Git-Live/git-live
+ * @since      Class available since Release 1.0.0
+ */
+
+namespace GitLive;
+
+/**
+ * @category   GitCommand
+ * @package    GitLive
+ * @subpackage GitLiveFlow
+ * @author     akito<akito-artisan@five-foxes.com>
+ * @author     suzunone<suzunone.eleven@gmail.com>
+ * @copyright Project Git Live
+ * @license MIT
+ * @version    GIT: $Id$
+ * @link https://github.com/Git-Live/git-live
+ * @see https://github.com/Git-Live/git-live
+ * @since      Class available since Release 1.0.0
+ */
+class exception extends \exception
+{
+
+}
+
