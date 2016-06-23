1880f1c12ac98a1efde0e49c11442800e3c09020
Suzunone <suzunone.eleven@gmail.com>
2016-06-23 11:41:03 +0900

    不要ファイルの整理

--- compile/src/stub.php
+++ compile/src/stub.php
@@ -1,5 +1,18 @@
 #!/usr/bin/env php
 <?php
+/**
+ * @category   GitCommand
+ * @package    GitLive
+ * @subpackage GitLiveCompile
+ * @author     akito<akito-artisan@five-foxes.com>
+ * @author     suzunone<suzunone.eleven@gmail.com>
+ * @copyright Project Git Live
+ * @license MIT
+ * @version    GIT: $Id$
+ * @link https://github.com/Git-Live/git-live
+ * @see https://github.com/Git-Live/git-live
+ * @since      Class available since Release 1.0.0
+ */
 define('GIT_LIVE_INSTALL_DIR', __FILE__);
 define('GIT_LIVE_VERSION', 'phar');
 Phar::mapPhar( 'git-live.phar' );
