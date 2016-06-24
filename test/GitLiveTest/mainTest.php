1be8df0eb5a2a842271d2f1a5dc14d0a40d641f7
Suzunone <suzunone.eleven@gmail.com>
2016-06-25 00:05:10 +0900

    test add

---
+++ test/GitLiveTest/mainTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ *
+ *
+ *
+ * PHP versions 5
+ *
+ *
+ *
+ * @category   %%project_category%%
+ * @package    %%project_name%%
+ * @subpackage %%subpackage_name%%
+ * @author     %%your_name%% <%%your_email%%>
+ * @copyright  %%your_project%%
+ * @license    %%your_license%%
+ * @version    GIT: $Id$
+ * @link       %%your_link%%
+ * @see        http://www.enviphp.net/c/man/v3/core/unittest
+ * @since      File available since Release 1.0.0
+ * @doc_ignore
+ */
+
+
+/**
+ *
+ *
+ *
+ *
+ * @category   %%project_category%%
+ * @package    %%project_name%%
+ * @subpackage %%subpackage_name%%
+ * @author     %%your_name%% <%%your_email%%>
+ * @copyright  %%your_project%%
+ * @license    %%your_license%%
+ * @version    GIT: $Id$
+ * @link       %%your_link%%
+ * @see        http://www.enviphp.net/c/man/v3/core/unittest
+ * @since      File available since Release 1.0.0
+ * @doc_ignore
+ */
+class mainTest extends testCaseBase
+{
+    /**
+     * +-- 初期化
+     *
+     * @access public
+     * @return void
+     */
+    public function initialize()
+    {
+        $this->free();
+    }
+    /* ----------------------------------------- */
+
+    public function executeTest()
+    {
+        $this->assertTrue(True);
+    }
+
+
+    /**
+     * +-- 終了処理
+     *
+     * @access public
+     * @return void
+     */
+    public function shutdown()
+    {
+    }
+
+}
---
+++ test/testCaseBase.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * テストのベースクラス
+ *
+ *
+ * PHP versions 5
+ *
+ *
+ *
+ * @category   %%project_category%%
+ * @package    %%project_name%%
+ * @subpackage %%subpackage_name%%
+ * @author     %%your_name%% <%%your_email%%>
+ * @copyright  %%your_project%%
+ * @license    %%your_license%%
+ * @version    GIT: $Id$
+ * @link       %%your_link%%
+ * @see        http://www.enviphp.net/c/man/v3/core/unittest
+ * @since      File available since Release 1.0.0
+ * @doc_ignore
+ */
+
+define('APP_BASE', realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../apps/'.basename(dirname(__FILE__)).'/').DIRECTORY_SEPARATOR);
+
+/**
+ * テストのベースクラス
+ *
+ *
+ *
+ * @category   %%project_category%%
+ * @package    %%project_name%%
+ * @subpackage %%subpackage_name%%
+ * @author     %%your_name%% <%%your_email%%>
+ * @copyright  %%your_project%%
+ * @license    %%your_license%%
+ * @version    GIT: $Id$
+ * @link       %%your_link%%
+ * @see        http://www.enviphp.net/c/man/v3/core/unittest
+ * @since      File available since Release 1.0.0
+ * @doc_ignore
+ */
+class testCaseBase extends EnviTestCase
+{
+
+    /**
+     * +-- コンストラクタ
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+    /* ----------------------------------------- */
+
+    /**
+     * +-- 初期化
+     *
+     * @access public
+     * @return void
+     */
+    public function initialize()
+    {
+    }
+    /* ----------------------------------------- */
+
+
+    /**
+     * +-- 終了処理をする
+     *
+     * @access public
+     * @return void
+     */
+    public function shutdown()
+    {
+    }
+    /* ----------------------------------------- */
+
+
+}
+
