7037c62e09a745585455fe498afb45fd268187b4
Suzunone <suzunone.eleven@gmail.com>
2016-12-01 18:27:31 +0900

    version up

--- src/libs/GitLive/GitBase.php
+++ src/libs/GitLive/GitBase.php
@@ -30,7 +30,7 @@ namespace GitLive;
 class GitBase
 {
     protected $deploy_repository_name = 'deploy';
-    const VERSION                     = '0.1.8';
+    const VERSION                     = '0.1.9';
 
     /**
      * +-- 引数配列を返す
