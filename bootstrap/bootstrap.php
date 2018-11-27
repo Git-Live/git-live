46df575b17ad80de20034d82050d234e107c1d20
Suzunone <suzunone.eleven@gmail.com>
2018-11-28 01:14:21 +0900

    bugfix

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -26,8 +26,14 @@ if (!ini_get('date.timezone')) {
     date_default_timezone_set($TZ ? $TZ : 'Europe/London');
 }
 
+
 if (!defined('GIT_LIVE_VERSION')) {
-    define('GIT_LIVE_VERSION', 'cli');
+    if ('phar://' === substr(__DIR__, 0, 7)) {
+        define('GIT_LIVE_VERSION', 'phar');
+    } else {
+        define('GIT_LIVE_VERSION', 'cli');
+    }
+
 }
 
 define('RESOURCES_DIR', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'resources');
--- src/main.php
+++ src/main.php
@@ -21,7 +21,7 @@
 include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 if (!defined('GIT_LIVE_INSTALL_PATH')) {
-    define('GIT_LIVE_INSTALL_PATH', __FILE__);
+    define('GIT_LIVE_INSTALL_PATH', dirname(__DIR__));
 }
 
 try {
