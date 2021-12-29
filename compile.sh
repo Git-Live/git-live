582b64d1eace34a30c43b3c13f77da89bd881ce7
Suzunone <suzunone.eleven@gmail.com>
2021-12-29 14:45:23 +0900

    self update bugfix. fixphp8

--- compile.sh
+++ compile.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
+export PHP_PATH='php'
 
 ./download-box.sh
 
-php composer.phar install
+$PHP_PATH composer.phar install
 
 function restorePlatform {
-    php composer.phar config --unset platform
+    $PHP_PATH composer.phar config --unset platform
     mv -f composer.lock.back composer.lock || true
 }
 
@@ -25,15 +26,15 @@ fi
 msgfmt  -o  ./resources/lang/en_US/LC_MESSAGES/messages.mo ./resources/lang/en_US/LC_MESSAGES/messages.po
 msgfmt  -o  ./resources/lang/ja_JP/LC_MESSAGES/messages.mo ./resources/lang/ja_JP/LC_MESSAGES/messages.po
 
-php composer.phar config platform.php 7.1.0
+$PHP_PATH composer.phar config platform.php 7.1.0
 
 cp composer.lock composer.lock.back || true
 mv -f vendor vendor.back || true
 
-php composer.phar install --no-dev
-php composer.phar dump-autoload -a
+$PHP_PATH composer.phar install --no-dev
+$PHP_PATH composer.phar dump-autoload -a
 
-php box.phar compile -vvv
+$PHP_PATH box.phar compile -vvv
 
 rm -rf vendor
 mv -f vendor.back vendor || true
@@ -41,4 +42,4 @@ mv -f vendor.back vendor || true
 zip -r windows.zip ./bin
 restorePlatform
 
-php bin/git-live.phar
+$PHP_PATH bin/git-live.phar
